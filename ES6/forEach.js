ebeb02fb877bc2f8289896026aea061c736150a1
Derek Allred <derekmallred@gmail.com>
2017-08-22 15:47:42 -0600

    Finished .forEach helper method with examples

--- ES6/forEach.js
+++ ES6/forEach.js
@@ -1,3 +1,8 @@
+//forEach helper methods are good when you need to take each item in an array and do something with it
+//because each item will be passed into a function you define you can do very specific things with each
+//array item. A good example is taking an array of posts and checking for certain words of post IDs
+//then doing something when you find the kind of post your looking for.
+
 //.forEach array helper method
 var names = ['Derek', 'Sam', 'Jack', 'Jane'];
 
@@ -15,3 +20,20 @@ names.forEach( function (item) {
 names.forEach( item => {
   console.log(item);
 });
+
+//example
+let numbers1 = [5,4,2,1,3,6];
+let sum1 = 0;
+numbers1.forEach((num) => {
+  sum1 += num;
+});
+console.log(sum1);
+
+//example where you reference an outside function called adder inside the forEach method
+let numbers2 = [5,4,2,1,3,6];
+let sum2 = 0;
+function adder(num) {
+  sum2 += num;
+}
+numbers2.forEach(adder);
+console.log(sum2);
