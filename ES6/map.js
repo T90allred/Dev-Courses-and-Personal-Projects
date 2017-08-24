7f5b5865646e546f54d68073cb27e0cdfc54b5c7
Derek Allred <derekmallred@gmail.com>
2017-08-23 20:53:50 -0600

    added another example

--- ES6/map.js
+++ ES6/map.js
@@ -30,3 +30,13 @@ let makes = cars.map( (car) => {
 });
 
 console.log(makes);
+
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds = trips.map( trip => {
+   return trip.distance/trip.time;
+});
