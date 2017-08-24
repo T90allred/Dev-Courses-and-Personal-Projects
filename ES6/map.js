3c21bfed79fb9f02013ff48328210e273bb0b38b
Derek Allred <derekmallred@gmail.com>
2017-08-23 20:32:11 -0600

    added example of .map metod

--- ES6/map.js
+++ ES6/map.js
@@ -1,11 +1,15 @@
 let numbers = [1,2,3,4,5,6];
 let doubled = [];
+doubledNumbers = [];
 
 for (var i = 0; i < numbers.length; i++) {
-  doubled.push(numbers[i]);
+
+  doubled.push(numbers[i] * 2);
 }
 console.log(doubled);
 
-// let doubled = numbers.map( (num) => {
-//   num * 2;
-// });
+let doubledNumbers = numbers.map( (num) => {
+  return num * 2;
+});
+
+console.log(doubledNumbers);
