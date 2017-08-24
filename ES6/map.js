cffee86ef03d18e051d029e8cab97dbe002b8e71
Derek Allred <derekmallred@gmail.com>
2017-08-23 18:55:24 -0600

    Added old way of doing a .map method

---
+++ ES6/map.js
@@ -0,0 +1,11 @@
+let numbers = [1,2,3,4,5,6];
+let doubled = [];
+
+for (var i = 0; i < numbers.length; i++) {
+  doubled.push(numbers[i]);
+}
+console.log(doubled);
+
+// let doubled = numbers.map( (num) => {
+//   num * 2;
+// });
