4f3b058f321eedf5550d409ab3681e654fbd74ad
Derek Allred <derekmallred@gmail.com>
2017-08-23 21:07:53 -0600

    added comments to code

--- ES6/map.js
+++ ES6/map.js
@@ -1,28 +1,28 @@
+//.map is good for going through an array and getting a specific value and saving those values
+//to a new array
+
 let numbers = [1,2,3,4,5,6];
 let doubled = [];
 let doubledNumbers = [];
 
+//old way with for loop
 for (var i = 0; i < numbers.length; i++) {
 
   doubled.push(numbers[i] * 2);
 }
 console.log(doubled);
 
+// do the same thing with .map
 doubledNumbers = numbers.map( (num) => {
   return num * 2;
 });
 
 console.log(doubledNumbers);
 
+//example using .map
 let cars = [
-  {
-    make: 'Subaru',
-    model: 'Impreza'
-  },
-  {
-    make: 'Toyota',
-    model: 'Rav4'
-  }
+  {make: 'Subaru', model: 'Impreza'},
+  {make: 'Toyota',model: 'Rav4'}
 ]
 
 let makes = cars.map( (car) => {
@@ -31,6 +31,7 @@ let makes = cars.map( (car) => {
 
 console.log(makes);
 
+// another example using .map
 var trips = [
   { distance: 34, time: 10 },
   { distance: 90, time: 50 },
@@ -40,3 +41,13 @@ var trips = [
 var speeds = trips.map( trip => {
    return trip.distance/trip.time;
 });
+
+// last example using .map
+var paints = [ { color: 'red' }, { color: 'blue' }, { color: 'yellow' }];
+
+function pluck(array, property) {
+    let colors = array.map( paint => {
+        return paint[property];
+    });
+    return colors;
+}
