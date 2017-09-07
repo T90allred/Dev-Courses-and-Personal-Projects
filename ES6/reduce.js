a237fdaaa580360414ac9e2ad93f596dad825fa7
Derek Allred <derekmallred@gmail.com>
2017-09-07 09:59:05 -0600

    added practical example of .reduce

--- ES6/reduce.js
+++ ES6/reduce.js
@@ -25,3 +25,13 @@ primaryColors.reduce( (previous, primaryColor) => {
 
 
 //practical example using .reduce
+function balancedParens(string) {
+  return !string.split('').reduce( (prev, char) => {
+    if(prev < 0) {return prev;}
+    if(char === '(') {return ++prev;}
+    if(char === ')') {return --prev;}
+    return prev;
+  }, 0);
+};
+
+balancedParens('((()');
