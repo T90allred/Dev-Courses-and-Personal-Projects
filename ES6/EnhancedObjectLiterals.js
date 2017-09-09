14eeaeac000d3577b89e835467672893ab9b37f9
Derek Allred <derekmallred@gmail.com>
2017-09-09 11:19:46 -0600

    added Enhanced Object literals file

---
+++ ES6/EnhancedObjectLiterals.js
@@ -0,0 +1,54 @@
+//----------------------------------------------  OLD WAY  -------------------------
+function createBookShop(inventory) {
+  return {
+    inventory: inventory,
+    inventoryValue: function() {
+      return this.inventory.reduce( (total,book) => total+=book.price,0);
+    },
+    priceForTitle: function (title) {
+      return this.inventory.find( (book) => book.title === title).price;
+    }
+    // whatCanIAfford: function (amount) {
+    //   return this.inventory.map( (book) => book.price < amount).title;
+    // }
+  }
+}
+
+const inventory = [
+  {title: 'Harry Potter', price: 10},
+  {title: 'YDKJS', price: 15},
+  {title: 'Code Complete', price: 20}
+];
+
+const bookShop = createBookShop(inventory);
+
+console.log(bookShop.inventoryValue());
+console.log(bookShop.priceForTitle('YDKJS'));
+
+//----------------------------------------------  NEW WAY  -------------------------
+function createBookShop(inventory) {
+  return {
+    inventory,
+    inventoryValue() {
+      return this.inventory.reduce( (total,book) => total+=book.price,0);
+    },
+    priceForTitle(title) {
+      return this.inventory.find( (book) => book.title === title).price;
+    },
+    whatCanIAfford(amount) {
+      return this.inventory.filter( (book) => book.price <= amount);
+    }
+  }
+}
+
+const inventory = [
+  {title: 'Harry Potter', price: 10},
+  {title: 'YDKJS', price: 15},
+  {title: 'Code Complete', price: 20}
+];
+
+const bookShop = createBookShop(inventory);
+
+console.log(bookShop.inventoryValue());
+console.log(bookShop.priceForTitle('YDKJS'));
+console.log(bookShop.whatCanIAfford(19));
