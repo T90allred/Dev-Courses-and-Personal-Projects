1e1aa14d8df71dfff027955f9c596560d3af0dbc
Derek Allred <derekmallred@gmail.com>
2017-09-11 20:07:55 -0600

    added an example using default function args

--- ES6/defaultFunctionArgs.js
+++ ES6/defaultFunctionArgs.js
@@ -9,3 +9,27 @@ function makeAjaxRequest(url, method = 'GET') {
 
 makeAjaxRequest('google.com');
 makeAjaxRequest('google.com', 'POST');
+
+
+//-----------------------------------------------------------------------------
+
+function User(id, admin) {
+  this.id = id;
+  this.admin = false;
+}
+
+function generateId() {
+  return Math.random() * 9999999;
+}
+
+function createAdmin(user = new User(generateId())) {
+  user.admin = true;
+  return user;
+}
+
+const user = new User(generateId());
+
+console.log('Not an admin', user);
+console.log('Is  an admin', createAdmin(user));
+console.log('Is an admin', createAdmin());
+console.log('Is an admin', createAdmin());
