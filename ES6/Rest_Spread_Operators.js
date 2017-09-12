3ed5b24739f24acb3d86c6a49b57434c5a5dbf18
Derek Allred <derekmallred@gmail.com>
2017-09-11 20:47:47 -0600

    added example using REST and SPREAD

--- ES6/Rest_Spread_Operators.js
+++ ES6/Rest_Spread_Operators.js
@@ -24,3 +24,31 @@ function addNumbers(...array) {
 addNumbers(1,2,3,4,5);
 
 //----------------------------SPREAD operator!!!----------------------------
+const defaultColors = ['red', 'green', 'orange'];
+const userFavoriteColors = ['yellow','blue','salmon'];
+
+let allColors = [ 'grey', ...defaultColors, ...userFavoriteColors ];
+
+//----------------------------COMBINE BOTH!!!----------------------------
+function validateShoppingList(...items) {
+  if(items.indexOf('milk') < 0) {
+    return [...items, 'milk'];
+  }
+  return items;
+}
+
+console.log(validateShoppingList('oranges','bananas','carrots'));
+
+//----------------------------PRACTICAL EXAMPLE!!!----------------------------
+const MathLibrary = {
+  calculateProduct(...rest) {
+    console.log('This method is being depricated, please use "multiply" in the future');
+    return this.multiply(...rest);
+  },
+  multiply(a, b) {
+    return a * b;
+  }
+};
+
+MathLibrary.calculateProduct(1,4);
+MathLibrary.multiply(1,2);
