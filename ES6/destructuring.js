f291712a3eff2b69d2a97282db99057127ea4a5e
Derek Allred <derekmallred@gmail.com>
2017-09-13 19:43:13 -0600

    added more destructuring examples

--- ES6/destructuring.js
+++ ES6/destructuring.js
@@ -10,3 +10,40 @@ var cost = expense.cost;
 //ES6
 //Here the code is setting an object 'expense' and making two properties on the object of 'type', 'cost'.
 const {type,cost} = expense;
+
+// simple example
+let person = {
+  name: 'Derek',
+  age: 27,
+  married: true,
+  kids: 0
+}
+
+
+const {name,age,married,kids} = person;
+
+console.log(name);
+console.log(age);
+console.log(married);
+console.log(kids);
+
+//other example using old way
+var savedFile = {
+  extension: 'jpg',
+  name: 'catPicture',
+  size: 1423
+}
+
+function formatName({name,extension,size}) {
+  return `The file ${name}.${extension} is of size ${size}`;
+}
+
+formatName(savedFile);
+
+// destructuring arrays
+const companies = [
+  'Google',
+  'Microsoft',
+  'Jane.com',
+  'Pluralsight'
+];
