f693d657b2b22d8cc2b52f6f698dfb63b078874e
Derek Allred <derekmallred@gmail.com>
2017-09-19 19:55:57 -0600

    did more with global object in node and interval functions

--- learnNode/app.js
+++ learnNode/app.js
@@ -1,1 +1,22 @@
-console.log('Hey Node!');
+// console.log('Hey Node!');
+
+// setTimeout(function() {
+//   console.log('3 seconds have passed!')
+// },3000);
+//
+// let timer = 0;
+// setInterval( () => {
+//   timer += 2;
+//   console.log(timer + ' seconds have passed');
+// },2000)
+let counter = 0;
+let timer = setInterval( () => {
+  counter += 2;
+  console.log(counter + ' seconds have passed');
+  if(counter >= 6) {
+    clearInterval(timer);
+  }
+},2000)
+
+console.log(__dirname);
+console.log(__filename);
