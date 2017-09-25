840ce602f16b1aec5e8f66437e77972eb3337886
Derek Allred <derekmallred@gmail.com>
2017-09-24 23:42:13 -0600

    Added a server and user component

--- aboutAngular4/src/app/app.module.ts
+++ aboutAngular4/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import {UserComponent} from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
