d189015a350d6c4ce5ef32840fde64e749e08381
Derek Allred <derekmallred@gmail.com>
2017-09-26 10:38:37 -0600

    Setup a customer interface

---
+++ fullThrottleAngular/src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,36 @@
+import { Component } from "@angular/core";
+import { Customer } from "./Customer";
+@Component({
+    selector: 'sandbox',
+    templateUrl: './sandbox.component.html',
+})
+
+export class SandboxComponent{
+    customer:Customer;
+    customers:Customer[];
+    constructor() {
+        this.customer = {
+            id: 3726,
+            name: 'Derek',
+            email: 'test@123.com'
+        },
+        this.customers = [
+        {
+            id: 3726,
+            name: 'Derek',
+            email: 'test1@123.com'
+        },
+        {
+            id: 3425,
+            name: 'Sam',
+            email: 'test2@123.com'
+        },
+        {
+            id: 2837,
+            name: 'Jake',
+            email: 'test3@123.com'
+        },
+        ]
+    }
+}
+
